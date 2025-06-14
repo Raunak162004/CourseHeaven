899b82703de04974a2697a1724a23c690f3d6820
Raunak Malhotra <rm1273503@gmail.com>
2025-06-14 18:02:57 +0530

    completed setting up the user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+
+const userSechema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+const User = mongoose.model('User', userSechema);
+
+export default User;
