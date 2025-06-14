f031e9548ff8696042238bf5c89d91c70d390600
Raunak Malhotra <rm1273503@gmail.com>
2025-06-14 10:32:39 +0530

    done with DB connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import db from './config/db.js';
 
 const app = express();
+
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -9,6 +11,8 @@ const port = process.env.PORT || 3000;
 app.get('/', (req,res) => {
     res.send('Hello, World!');
 })
+
+db();
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
