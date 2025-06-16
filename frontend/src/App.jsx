9075b99428e7a733a4c56a46b928c48eb1594fdc
Raunak Malhotra <rm1273503@gmail.com>
2025-06-16 08:42:37 +0530

    routing setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
+      </Routes>
+    </>
   )
 }
 
